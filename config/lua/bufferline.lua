da82a561f8c3896ed9e4d33ed17991b2e7992d1b
oliverwiegers <oliver.wiegers@gmail.com>
2024-01-12 07:31:22 +0100

    Configure bufferline visuals and keybindings

--- config/lua/bufferline.lua
+++ config/lua/bufferline.lua
@@ -1,2 +1,21 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup{
+  options = {
+    mode = 'tabs',
+    separator_style = 'slope',
+    diagnostics = "nvim_lsp",
+    sort_by = 'tabs',
+
+    hover = {
+      enabled = false
+    },
+
+    diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and " " or ""
+        return " " .. icon .. count
+    end,
+    name_formatter = function(buf)
+      return buf.tabnr .." " .. buf.name
+    end,
+  }
+}
--- config/lua/init.lua
+++ config/lua/init.lua
@@ -101,9 +101,21 @@ vim.keymap.set("n", "<C-l>", ":nohlsearch<CR><C-l>", {silent=true})
 
 -- FZF commands.
 vim.keymap.set("n", "<Tab><Tab>", ":Rg<CR>", {silent=true})
-vim.keymap.set("n", "<Leader>b", ":Buffers<CR>", {silent=true})
+vim.keymap.set("n", "<Leader><Enter>", ":Buffers<CR>", {silent=true})
 vim.keymap.set("n", "<Leader><Space>", ":Files<CR>", {silent=true})
 vim.keymap.set("n", "<Leader>gc", ":Commits<CR>", {silent=true})
+
+-- Bufferline commands.
+vim.keymap.set("n", "<Leader>p", ":BufferLinePick<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>1", ":BufferLineGoToBuffer 1<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>2", ":BufferLineGoToBuffer 2<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>3", ":BufferLineGoToBuffer 3<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>4", ":BufferLineGoToBuffer 4<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>5", ":BufferLineGoToBuffer 5<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>6", ":BufferLineGoToBuffer 6<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>7", ":BufferLineGoToBuffer 7<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>8", ":BufferLineGoToBuffer 8<CR>", {silent=true})
+vim.keymap.set("n", "<Leader>9", ":BufferLineGoToBuffer 9<CR>", {silent=true})
 
 -- Open fuzzy file finder if not a file is given at startup.
 if vim.fn.argc() == 0 then
