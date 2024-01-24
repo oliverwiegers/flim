20267ce061acba6a1a61a94de2891b7559666e5b
oliverwiegers <oliver.wiegers@netlogix.de>
2024-01-24 07:56:52 +0100

    Reconfigure lsp setup

--- config/lua/nvim-cmp.lua
+++ config/lua/nvim-cmp.lua
@@ -104,35 +104,3 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- Python
-require('lspconfig').pyright.setup {
-  capabilities = capabilities,
-}
-
--- Lua
-require('lspconfig').lua_ls.setup {
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-    },
-  },
-}
-
--- Nix
-require('lspconfig').nil_ls.setup {
-  autostart = true,
-  capabilities = capabilities,
-}
-
--- Markdown
-require("lspconfig").marksman.setup{
-  capabilities = capabilities,
-}
---
+++ config/lua/nvim-lspconfig.lua
@@ -0,0 +1,39 @@
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- Python
+require('lspconfig').pyright.setup {
+  capabilities = capabilities,
+}
+
+-- Lua
+require('lspconfig').lua_ls.setup {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+    },
+  },
+}
+
+-- Nix
+require('lspconfig').nil_ls.setup {
+  autostart = true,
+  capabilities = capabilities,
+}
+
+-- Markdown
+require("lspconfig").marksman.setup{
+  capabilities = capabilities,
+}
+
+require("lspconfig").terraformls.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
