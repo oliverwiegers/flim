a7aac19077e592dd3074ee7c0023312f64cacb52
oliverwiegers <oliver.wiegers@netlogix.de>
2024-05-10 15:03:41 +0200

    Remove mistake function

--- config/lua/functions.lua
+++
@@ -1,9 +0,0 @@
--- Open fuzzy file finder if not a file is given at startup.
-if vim.fn.argc() == 0 then
-    vim.defer_fn(
-        function ()
-            vim.cmd('Files')
-        end,
-        0
-    )
-end
